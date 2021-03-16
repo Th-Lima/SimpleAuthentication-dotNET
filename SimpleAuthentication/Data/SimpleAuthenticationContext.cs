43a1a629a8aaef9209dc5f83ac8ecf379531a619
Thales Lima <lthales53@gmail.com>
2021-03-16 20:39:33 -0300

    Connecting with database

---
+++ SimpleAuthentication/Data/SimpleAuthenticationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAuthentication.Models;
+using System.Collections.Generic;
+
+namespace SimpleAuthentication.Data
+{
+    public class SimpleAuthenticationContext : DbContext
+    {
+        public SimpleAuthenticationContext(DbContextOptions<SimpleAuthenticationContext> options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new List<User>()
+            {
+                new User(1, "User01", "123456", true),
+                new User(2, "User02", "123456", false)
+            });
+        }
+    }
+}
---
+++ SimpleAuthentication/Migrations/20210316233331_Initial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleAuthentication.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true),
+                    IsAdmin = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "IsAdmin", "Name", "Password" },
+                values: new object[] { 1, true, "User01", "123456" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "IsAdmin", "Name", "Password" },
+                values: new object[] { 2, false, "User02", "123456" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
