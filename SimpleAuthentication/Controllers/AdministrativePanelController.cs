2c935abd8614963468c627b1df37e43b8e408f10
Thales Lima <lthales53@gmail.com>
2021-03-18 19:21:23 -0300

    Implementing Controllers

---
+++ SimpleAuthentication/Controllers/AdministrativePanelController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAuthentication.Lib.Filter;
+using SimpleAuthentication.Lib.Login;
+using SimpleAuthentication.Models;
+
+namespace SimpleAuthentication.Controllers
+{
+    public class AdministrativePanelController : Controller
+    {
+        public UserLogin _userLogin { get; set; }
+
+        public AdministrativePanelController(UserLogin userLogin)
+        {
+            _userLogin = userLogin;
+        }
+
+        [UserAuthorization]
+        public IActionResult Index()
+        {
+            User user = _userLogin.GetUser();
+            if (user.IsAdmin)
+            {
+                return View();
+            }
+            else
+            {
+                TempData["MSG_E"] = "Você não tem acesso ao painel administrativo, pois não é um administrador.";
+                return RedirectToAction("Authenticated", "Login");
+            }
+        }
+    }
+}
--- SimpleAuthentication/Controllers/LoginController.cs
+++ SimpleAuthentication/Controllers/LoginController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAuthentication.Lib.Filter;
+using SimpleAuthentication.Lib.Login;
 using SimpleAuthentication.Models;
+using SimpleAuthentication.Repositories.Contracts;
 using System.Diagnostics;
 
 namespace SimpleAuthentication.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        private IUserRepository _userRepository;
+        private UserLogin _userLogin;
+
+        public LoginController(IUserRepository userRepository, UserLogin userLogin)
+        {
+            _userRepository = userRepository;
+            _userLogin = userLogin;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromForm] User user)
+        {
+            User userDb = _userRepository.Login(user.Name, user.Password);
+
+            if(userDb != null)
+            {
+                _userLogin.Login(userDb);
+
+                return RedirectToAction("Authenticated", "Login");
+            }
+            else
+            {
+                ViewData["MSG_E"] = "Senha ou nome incorreto, por favor, digite novamente";
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            _userLogin.Logout();
+
+            return RedirectToAction("Login", "Login");
+        }
+
+        [UserAuthorization]
+        public IActionResult Authenticated()
         {
             return View();
         }
